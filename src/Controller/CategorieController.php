4c6bfcead505cbb9e6094e83c7cc22861f462754
jessie-durand <jessie_durand@hotmail.fr>
2022-01-21 10:53:47 +0100

    oooo

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -50,7 +50,7 @@ class CategorieController extends AbstractController
     }
 
     #[Route('/{id}', name: 'categorie_show', methods: ['GET', 'POST'])]
-    public function show(Request $request, Categorie $categorie, ProduitRepository $produitRepository, string $tag): Response
+    public function show(Request $request, Categorie $categorie, ProduitRepository $produitRepository): Response
     {
         $form = $this->createForm(SearchBarType::class);
         $form->handleRequest($request);
@@ -66,17 +66,28 @@ class CategorieController extends AbstractController
             'produits' => $produits,
             'form' => $form->createView(),
             'categorie' => $categorie,
-            // 'tagGet' => $tag,
         ]);
     }
 
 
     #[Route('/{id}/{tag}', name: 'tag_show', methods: ['GET'])]
-    public function showTag(Categorie $categorie, string $tag): Response
+    public function showTag(Request $request, Categorie $categorie, string $tag, ProduitRepository $produitRepository): Response
     {
+        $form = $this->createForm(SearchBarType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $search = $form->getData()['search'];
+            $produits = $produitRepository->findLikeName($search);
+        } else {
+            $produits = $produitRepository->findAll();
+        }
+
         return $this->render('categorie/showTag.html.twig', [
             'categorie' => $categorie,
-            'tagGet' => $tag
+            'tagGet' => $tag,
+            'form' => $form->createView(),
+            'produits' => $produits,
         ]);
     }
 }
